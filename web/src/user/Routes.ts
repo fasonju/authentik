7337e4c14ba57679a7f116e3f124569a3f0fe017
Teffen Ellis <teffen@goauthentik.io>
2025-05-29 16:38:51 +0200

    web: Use aliases for dynamic imports.
    This reverts commit a536712876a01324bb6c160bcd2503066754f1ab.

--- web/src/user/Routes.ts
+++ web/src/user/Routes.ts
@@ -9,7 +9,7 @@ export const ROUTES: Route[] = [
     new Route(new RegExp("^#.*")).redirect("/library"),
     new Route(new RegExp("^/library$"), async () => html`<ak-library></ak-library>`),
     new Route(new RegExp("^/settings$"), async () => {
-        await import("@goauthentik/user/user-settings/UserSettingsPage");
+        await import("#user/user-settings/UserSettingsPage");
         return html`<ak-user-settings></ak-user-settings>`;
     }),
 ];
